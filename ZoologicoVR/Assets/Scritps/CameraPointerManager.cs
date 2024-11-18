5d73a6a0e3277cc44e3b126347fe2447b70f9efc
Sebastian Garrido <sgarrido668@gmail.com>
2024-11-18 18:15:25 -0300

    Pointer

---
+++ ZoologicoVR/Assets/Scritps/CameraPointerManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPointerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pointer;
+    [SerializeField] private float maxDistancePointer = 4.5f;
+    [SerializeField] private float distancePointerObject = 0.95f;
+    [Range(0,1)]
+    private const float _maxDistance = 10;
+    private GameObject _gazedAtObject = null;
+
+    private readonly string interactableTag = "Interactable";
+    private float scaleSize = 0.025f;
+
+    private void Start()
+    {
+        GazeManager.Instance.OnGazeSelection += GazeSelection;
+    }
+
+    private void GazeSelection()
+    {
+        _gazedAtObject?.SendMessage("OnPointerClick", null, SendMessageOptions.DontRequireReceiver);
+    }
+
+    public void Update()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
+        {
+
+            if (_gazedAtObject != hit.transform.gameObject)
+            {
+
+                _gazedAtObject?.SendMessage("OnPointerExit", null, SendMessageOptions.DontRequireReceiver);
+                _gazedAtObject = hit.transform.gameObject;
+                _gazedAtObject.SendMessage("OnPointerEnter", null, SendMessageOptions.DontRequireReceiver);
+                GazeManager.Instance.StartGazeSelection();
+            }
+            if (hit.transform.CompareTag(interactableTag))
+            {
+                PointerOnGaze(hit.point);
+            }
+            else
+            {
+                PointerOutGaze();
+            }
+        }
+        else
+        {
+            _gazedAtObject?.SendMessage("OnPointerExit", null, SendMessageOptions.DontRequireReceiver);
+            _gazedAtObject = null;
+        }
+
+        if (Google.XR.Cardboard.Api.IsTriggerPressed)
+        {
+            _gazedAtObject?.SendMessage("OnPointerClick", null, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    private void PointerOnGaze(Vector3 hitpoint)
+    {
+        float scaleFactor = scaleSize * Vector3.Distance(transform.position, hitpoint);
+        pointer.transform.localScale = Vector3.one * scaleFactor;
+        pointer.transform.parent.position = CalculateVectorPosition(transform.position, hitpoint, distancePointerObject);
+    }
+
+    private void PointerOutGaze()
+    {
+        pointer.transform.localScale = Vector3.one * 0.1f;
+        pointer.transform.parent.transform.localPosition = new Vector3(0, 0, maxDistancePointer);
+        pointer.transform.parent.parent.transform.rotation = transform.rotation;
+        GazeManager.Instance.CancelGazeSelection();
+    }
+
+    private Vector3 CalculateVectorPosition(Vector3 po0, Vector3 p1, float t)
+    {
+        float x = po0.x + t * (p1.x - po0.x);
+        float y = po0.y + t * (p1.y - po0.y);
+        float z = po0.z + t * (p1.z - po0.z);
+
+        return new Vector3(x, y, z);
+    }
+}
